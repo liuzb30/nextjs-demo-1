1bb91172748ae121c9186e3d3f4cc00e1f68fde7
liu zhenbin <536543269@qq.com>
2020-12-23 15:51:53 +0800

    分页基本完成

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,30 +2,46 @@ import {GetServerSideProps, NextPage} from "next";
 import Link from "next/link";
 import {Post} from "../../src/entity/Post";
 import {getDatabaseConnection} from "../../lib/connection";
+import * as querystring from "querystring";
 
 type Props = {
-    posts: Post[]
+    posts: Post[];
+    page: number;
+    totalPage: number;
 }
 const PostsIndex: NextPage<Props> = (props) => {
-    const {posts} = props
+    const {posts, page, totalPage} = props
     return (
         <div>
             <h1>
                 文章列表
             </h1>
             {
-                posts.map(post => <div key={post.id}><Link href={`/posts/${post.id}`}><a>{post.title}</a></Link> </div>)
+                posts.map(post => <div key={post.id}><Link href={`/posts/${post.id}`}><a>{post.title}</a></Link></div>)
             }
+            {page > 1 && <Link href={`/?page=${page - 1}`}><a>上一页</a></Link>}
+            {page > 1 && page < totalPage && ' | '}
+            {page < totalPage && <Link href={`/?page=${page + 1}`}><a>下一页</a></Link>}
         </div>
     )
 }
 export default PostsIndex;
-export const getServerSideProps:GetServerSideProps = async () => {
-    const connection  = await getDatabaseConnection()
-    const posts = await connection.manager.find(Post)
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const index = context.req.url.indexOf('?')
+    let page = 1
+    if(index!==-1){
+        const queryStr = querystring.parse(context.req.url.slice(index + 1))
+        page = Number(queryStr.page) || 1
+    }
+
+    const perPage = 1
+    const connection = await getDatabaseConnection()
+    const [posts, totalPage] = await connection.manager.findAndCount(Post, {skip: perPage * (page - 1), take: perPage})
     return {
         props: {
-            posts: JSON.parse(JSON.stringify(posts))
+            posts: JSON.parse(JSON.stringify(posts)),
+            page,
+            totalPage
         }
     }
 }
