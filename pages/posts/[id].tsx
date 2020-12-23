19c1203d5d8fa97a956e5549313642d636bbcab2
liu zhenbin <536543269@qq.com>
2020-12-23 17:55:08 +0800

    文章详情 css

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,7 @@
 import {GetServerSideProps, NextPage} from "next";
 import {getDatabaseConnection} from "../../lib/connection";
 import {Post} from "../../src/entity/Post";
+import marked from "marked";
 
 type Props = {
     post: Post
@@ -8,19 +9,30 @@ type Props = {
 const PostDetail: NextPage<Props> = (props) => {
     const {post} = props
     return (
-        <div>
+        <div className='wrapper'>
             <h1>{post?.title}</h1>
-            <article dangerouslySetInnerHTML={{__html: post?.content}}></article>
+            <article className="markdown-body" dangerouslySetInnerHTML={{__html: marked(post?.content)}}></article>
+            <style jsx>{`
+            .wrapper {
+              max-width: 800px;
+              margin: 16px auto;
+              padding: 0 16px;
+            }
+            h1 {
+              padding-bottom: 16px;
+              border-bottom: 1px solid #666;
+            }
+           `}</style>
         </div>
     )
 }
 
 export default PostDetail
-export const getServerSideProps:GetServerSideProps<any, {id:string}> = async (context)=>{
+export const getServerSideProps: GetServerSideProps<any, { id: string }> = async (context) => {
     const connection = await getDatabaseConnection()
     const post = await connection.manager.findOne('posts', context.params.id)
-    return{
-        props:{
+    return {
+        props: {
             post: JSON.parse(JSON.stringify(post))
         }
     }
