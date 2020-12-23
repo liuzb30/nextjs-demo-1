fa9618adaddbd6a39f4a2d1855b1a1403d20261d
liu zhenbin <536543269@qq.com>
2020-12-23 17:31:35 +0800

    首页css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,31 @@
-import PostsIndex, {getServerSideProps} from "./posts";
+import {NextPage} from "next";
+import Link from "next/link";
 
-export default PostsIndex;
-export {getServerSideProps};
+const Home: NextPage = () => {
+    return (
+        <>
+            <div className='cover'>
+                <img src='/logo.png'/>
+                <h1>刘圳槟的个人博客</h1>
+                <p>我是一个爱学习的人</p>
+                <p><Link href='/post'><a>文章列表</a></Link></p>
+            </div>
+            <style jsx>{`
+              .cover{
+                height: 100vh;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                img{
+                  width: 120px;
+                  height: 120px;
+                  border-radius: 50%;
+                }
+              }
+            `}</style>
+        </>
+    )
+}
+
+export default Home
