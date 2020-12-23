e44d3100115a925096a7fb318df6bd3a197a0473
liu zhenbin <536543269@qq.com>
2020-12-23 14:28:03 +0800

    使用useForm替代Form

--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -1,46 +1,34 @@
 import {GetServerSideProps, GetServerSidePropsContext, NextPage} from "next";
-import {useCallback, useState} from "react";
 import axios from "axios";
 import {withSession} from "../lib/withSession";
 import {User} from "../src/entity/User";
-import Form from "../components/Form";
+import {useForm} from "../hooks/useForm";
 
 type Props = {
     user:User
 }
 const SignIn: NextPage<Props> = (props) => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    })
-    const initErrors = {
-        username:[] as string[],
-        password:[] as string[]
-    }
-    const [errors, setErrors] = useState(initErrors)
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
-        setErrors(initErrors)
-        axios.post('/api/v1/session',formData).then(res=>{
-            window.alert('登录成功')
-        }).catch(e=>{
-            if(e.response.status===401){
-                setErrors(e.response.data)
+    const initFormData = {username: '', password: ''}
+    const {form} = useForm({
+            initFormData,
+            fields:[
+                {label: '用户名', type: 'text', key: 'username',},
+                {label: '密码', type: 'password', key: 'password',},
+            ],
+            buttons: (<button type='submit'>登录</button>),
+            submit:{
+                request:formData => axios.post('/api/v1/session', formData),
+                message:'登录成功'
             }
-        })
-    }, [formData])
+        }
+    )
     return (
         <div>
             {props.user &&
             <div>
                 当前用户为{props.user.username}
             </div>}
-            <Form onSubmit={onSubmit} fields={[
-                {label:'用户名',type:'text',value:formData.username,
-                    onChange:e => setFormData({...formData, username: e.target.value}),errors:errors.username},
-                {label:'密码',type:'password',value:formData.password,
-                    onChange:e => setFormData({...formData, password: e.target.value}),errors:errors.password},
-            ]} buttons={<button type='submit'>登录</button>}/>
+            {form}
         </div>
     )
 }
--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -1,42 +1,27 @@
 import axios from "axios";
 import {NextPage} from "next";
-import {ChangeEventHandler, useCallback, useState} from "react";
-import Form from "../components/Form";
+import {useForm} from "../hooks/useForm";
 
 const SignUp: NextPage = () => {
-    const [formData, setFormData] = useState({
-        username: '',
-        password: '',
-        passwordConfirm: ''
-    })
-    const initErrors = {
-        username:[] as string[],
-        password:[] as string[],
-        passwordConfirm:[] as string[]
-    }
-    const [errors, setErrors] = useState(initErrors)
-
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
-        setErrors(initErrors)
-        axios.post('/api/v1/users',formData).then(res=>{
-            window.alert('注册成功')
-        }).catch(e=>{
-            if(e.response.status===401){
-                setErrors(e.response.data)
+    const initFormData = {username: '', password: '',passwordConfirm:''}
+    const {form} = useForm({
+            initFormData,
+            fields:[
+                {label: '用户名', type: 'text', key: 'username',},
+                {label: '密码', type: 'password', key: 'password'},
+                {label: '确认密码', type: 'password', key: 'passwordConfirm'},
+            ],
+            buttons: (<button type='submit'>注册</button>),
+            submit:{
+                request:formData => axios.post('/api/v1/users', formData),
+                message:'注册成功'
             }
-        })
-    }, [formData])
+        }
+    )
+
     return (
         <div>
-            <Form onSubmit={onSubmit} fields={[
-                {label:'用户名',type:'text',value:formData.username,
-                    onChange:e => setFormData({...formData, username: e.target.value}),errors:errors.username},
-                {label:'密码',type:'password',value:formData.password,
-                    onChange:e => setFormData({...formData, password: e.target.value}),errors:errors.password},
-                {label:'确认密码',type:'password',value:formData.passwordConfirm,
-                    onChange:e => setFormData({...formData, passwordConfirm: e.target.value}),errors:errors.passwordConfirm},
-            ]} buttons={<button type='submit'>注册</button>}/>
+            {form}
         </div>
     )
 }
